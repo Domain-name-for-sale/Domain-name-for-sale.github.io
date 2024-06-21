da4a5b5edcf8e5d4cf8dbbf179ddd6665f61c322
Domain-name-for-sale <domain@188mail.top>
2024-06-21 17:44:23 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,228 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <title><?php echo $_SERVER['HTTP_HOST'];?>需要您提供报价</title>
+  <link rel="icon" type="image/x-icon" href="https://img.alicdn.com/imgextra/i2/2/O1CN01SGLtaC1EA1jmhL8ef_!!2-rate.png" />
+  <meta name="description" content="www.lanmitu.com" />
+  <meta name="keywords" content="www.lanmitu.com" />
+  <meta http-equiv="Content-Type" content="text/html; charset=gb2312" />
+
+  <style>
+    body {
+      line-height: 1.6;
+      background-color: #fff;
+    }
+
+    body,
+    th,
+    td,
+    button,
+    input,
+    select,
+    textarea {
+      font-family: "Microsoft Yahei", "Hiragino Sans GB", "Helvetica Neue", Helvetica, tahoma, arial, verdana, sans-serif, "WenQuanYi Microhei", "\5B8B\4F53";
+      font-size: 12px;
+      color: #666;
+      -webkit-font-smoothing: antialiased;
+      -moz-font-smoothing: antialiased;
+    }
+
+    html,
+    body {
+      height: 100%;
+    }
+
+    html,
+    body,
+    h1,
+    h2,
+    h3,
+    h4,
+    h5,
+    h6,
+    hr,
+    p,
+    iframe,
+    dl,
+    dt,
+    dd,
+    ul,
+    ol,
+    li,
+    pre,
+    form,
+    button,
+    input,
+    textarea,
+    th,
+    td,
+    fieldset {
+      margin: 0;
+      padding: 0;
+    }
+
+  .wrap {
+      margin: 0px auto;
+      min-width: 990px;
+      max-width: 1190px
+    }
+
+  .banner1 {
+      background: url(https://img.alicdn.com/imgextra/i1/0/O1CN01yCx7761EA1jmo09Qm_!!0-rate.jpg) no-repeat 50% top;
+      margin: 0 auto;
+      height: 343px;
+      text-align: center
+    }
+
+  .main-out {
+      margin: 0 auto;
+      padding: 0 0 20px 0
+    }
+
+  .orange-btn {
+      background-color: #ff8400;
+      display: inline-block;
+      padding: 0px 20px;
+      color: #fff;
+      height: 50px;
+      line-height: 50px;
+      font-size: 18px;
+      border-radius: 3px;
+      margin-top: 20px
+    }
+
+  .orange-btn:hover {
+      color: #fff;
+      background-color: #f16600;
+    }
+
+  .banner1 h1 {
+      font-size: 48px;
+      color: #feff07;
+      font-family: Tahoma, sans-serif;
+      padding: 60px 0 20px 0
+    }
+
+  .banner1 p {
+      color: #fff;
+      font-size: 20px;
+    }
+
+  .domain-con {
+      padding: 20px 50px;
+      position: relative;
+    }
+
+  .left {
+      background: #f6f6f6 url(https://img.alicdn.com/imgextra/i3/0/O1CN01grZLHX1EA1jmgy2hx_!!0-rate.jpg) no-repeat left top;
+      height: 100%;
+      width: 100%;
+      margin-right: 320px;
+      font-size: 16px
+    }
+
+  .domainout {
+      padding: 50px 380px 50px 100px
+    }
+
+  .domainout p {
+      line-height: 50px;
+    }
+
+  .domainout p span {
+      display: inline-block;
+      width: 120px;
+      text-align: right;
+      margin-right: 5px
+    }
+
+    strong.red {
+      color: #ff7e00;
+      font-size: 24px
+    }
+
+  .right {
+      background-color: #2780d9;
+      height: 100%;
+      width: 320px;
+      position: absolute;
+      right: 50px;
+      top: 0;
+      color: #fff;
+      padding: 0px 20px 0px 20px;
+      font-size: 16px
+    }
+
+  .right h2 {
+      color: #fff;
+      border-bottom: 1px solid #1071d2;
+      height: 56px;
+      line-height: 56px;
+      font-size: 20px;
+      text-align: center
+    }
+
+  .imgpic {
+      padding: 25px 0 20px 0
+    }
+
+  .contact {
+      margin-left: 50px
+    }
+
+  .contact p {
+      line-height: 40px
+    }
+
+    a {
+      text-decoration: none;
+    }
+
+  .text-c {
+      text-align: center;
+    }
+  </style>
+</head>
+
+<body>
+  <div class="banner-out">
+    <div class="banner1">
+      <div class="wrap">
+        <h1><?php echo $_SERVER['HTTP_HOST'];?></h1>
+        <p>在任何时间任何地点您都可以向我发送电子邮件提供域名您对当前域名的报价<br>At any time and anywhere you can send me an email with a domain name and your quote for the current domain name.</p>
+      </div>
+    </div>
+  </div>
+  <div class="main-out">
+    <div class="wrap">
+      <div class="domain-con">
+        <div class="left">
+          <div class="domainout">
+            <p><span>当前域名：</span><?php echo $_SERVER['HTTP_HOST'];?></p>
+            <p><span>交易类型：</span>需要您提供报价</p>
+            <p><span>域名类型：</span>精品三拼域名|<a href="http://www.wanmi.cc/zd/lantingshi.com" target="_blank" rel="nofollow">共匹配到 31 个公司企业终端</a></p>
+            <p><span>域名简介：</span>兰亭石|兰庭室|兰亭诗</p>
+            <p>
+                <a href="mailto:domainnamebot@gmail.com" class="orange-btn" target="_blank">立即给我发送电子邮件</a>ㅤㅤ<a href="mailto:domainnamebot@gmail.com" class="orange-btn" target="_blank">Send me an email now</a>
+            </p>
+          </div>
+        </div>
+        <div class="right">
+          <h2>联系我购买此域名</h2>
+          <div class="imgpic text-c">
+            <img src="https://img.alicdn.com/imgextra/i2/2/O1CN01SGLtaC1EA1jmhL8ef_!!2-rate.png" />
+            <p class="mt-10">域名所有人：FJJ<br>邮箱 Email<br>domainnamebot@gmail.com</p>
+          </div>
+          <div class="contact">
+            <!--<p class="mt-10">邮箱 Email</p>
+            <p class="mt-10">domainnamebot@gmail.com</p>-->
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</body>
+
+</html>
